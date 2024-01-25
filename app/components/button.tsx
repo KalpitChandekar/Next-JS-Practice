3207ab33c5dd339fcc9eb5203e3c9c5329549a5a
Kalpit <kalpitchandekar1736@gmail.com>
2024-01-25 07:31:21 +0530

    morning practice

--- app/components/button.tsx
+++
@@ -1,6 +0,0 @@
-const button = () => {
-  return (
-    <div>button</div>
-  )
-}
-export default button
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,30 @@
 "use client";
+import { useEffect, useState } from "react";
 
-import React, { useState } from "react";
-import UserCard from "./components/UserCard";
+const App = () => {
+  const [count, setCount] = useState(0);
 
-const Page: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
-  const [counts, setCounts] = useState<number>(0);
+  useEffect(() => {
+    alert("Good Morning");
+  },[]);
 
-  // Use useEffect to display an alert when the count changes
+  const IncNum = () => {
+    setCount(count + 2);
+    console.log("Good Morning")
+  };
 
   return (
-    <div className="flex justify-center items-center h-screen w-full gap-4">
-      <button
-        className="bg-blue-300 px-4 py-2 rounded-md font-semibold "
-        onClick={() => setCount(count + 1)}
-      >
-        Click {count}
-      </button>
-
-      <UserCard />
+    <div className="flex items-center h-screen w-full gap-4 ml-4">
+      <h1>{count}</h1>
 
       <button
-        className="bg-blue-300 px-4 py-2 rounded-md font-semibold"
-        onClick={() => setCounts(counts + 1)}
+        className="bg-blue-300 px-4 py-2 rounded-md font-semibold ml-2 "
+        onClick={() => IncNum()}
       >
-        Click {counts}
+        Click
       </button>
     </div>
   );
 };
 
-export default Page;
+export default App;
