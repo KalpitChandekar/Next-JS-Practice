5feeddd889bb22ff3b8e20580b137772a92092d3
Kalpit <kalpitchandekar1736@gmail.com>
2024-01-26 15:37:56 +0530

    fetch random API

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,66 @@
-const usersData = [
-  {
-    name: "John Doe",
-    age: 32,
-    address: "123, Charming Avenue",
-  },
-  {
-    name: "Clark Kent",
-    age: 25,
-    address: "456, Sunny Side Lane",
-  },
-  {
-    name: "Bruce Wayne",
-    age: 40,
-    address: "789, Elegant Street",
-  },
-  {
-    name: "Tony Stark",
-    age: 35,
-    address: "0123, Marvelous Boulevard",
-  },
-];
+"use client";
+import { useState, useEffect } from "react";
 
-const App = () => {
+// const usersData = [
+//   {
+//     name: "John Doe",
+//     age: 32,
+//     address: "123, Charming Avenue",
+//   },
+//   {
+//     name: "Clark Kent",
+//     age: 25,
+//     address: "456, Sunny Side Lane",
+//   },
+//   {
+//     name: "Bruce Wayne",
+//     age: 40,
+//     address: "789, Elegant Street",
+//   },
+//   {
+//     name: "Tony Stark",
+//     age: 35,
+//     address: "0123, Marvelous Boulevard",
+//   },
+// ];
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+const Page = () => {
+  const [count, setCount] = useState<User[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+      console.log(data);
+      setCount(data.users);
+    };
+    getUsers();
+  }, []);
+
   return (
-    <div className="flex items-center justify-center h-screen w-full">
-      {usersData.map((user, index) => {
+    <div className="flex flex-wrap items-center justify-center h-screen w-full">
+      {count?.map((user, index) => {
         return (
           <div
             key={index}
             className="border border-black p-4 m-4 rounded-lg bg-green-300"
           >
             <h1 className="text-xl">User</h1>
-            <p> name: {user.name}</p>
-            <p>age: {user.age}</p>
-            <p>address: {user.address}</p>
+            <p>ID:{user.id}</p>
+            <p>firstName:{user.firstName}</p>
+            <p>lastName:{user.lastName}</p>
+            <p>age:{user.age}</p>
           </div>
         );
       })}
     </div>
   );
 };
-export default App;
+export default Page;
